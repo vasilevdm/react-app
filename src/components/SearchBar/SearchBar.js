a12b03ad1bafde1467c35ed294790faf39b174d6
dmitr <vasilev.dm@gmail.com>
2018-09-12 16:05:42 +0300

    Add visual feedback and set states

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,11 +8,47 @@ const sortByOptions = {
 }
 
 class SearchBar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			term: '',
+			location: '',
+			sortBy: 'best_match'
+		};
+		this.handleTermChange = this.handleTermChange.bind(this);
+		this.handleLocationChange = this.handleLocationChange.bind(this);
+		this.handleSearch = this.handleSearch.bind(this);
+	}
 	renderSortByOptions() {
 		return Object.keys(sortByOptions).map( sortByOption => {
 			let sortByOptionValue = sortByOptions[sortByOption];
-			return <li key="{sortByOptionValue}">{sortByOption}</li>;
+			return <li onClick={this.handleSortByChange.bind(this, sortByOptionValue)} className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption}</li>;
 		} );
+	}
+	getSortByClass(sortByOption) {
+		if (this.state.sortBy === sortByOption)
+			return 'active';
+		else
+			return '';
+	}
+	handleSortByChange(newSortBy) {
+		this.setState({
+			sortBy: newSortBy
+		});
+	}
+	handleTermChange(event) {
+		this.setState({
+			term: event.target.value
+		})
+	}
+	handleLocationChange(event) {
+		this.setState({
+			location: event.target.value
+		})
+	}
+	handleSearch(event) {
+		this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+		event.preventDefault();
 	}
 	render() {
 		return (
@@ -23,11 +59,11 @@ class SearchBar extends React.Component {
 			    </ul>
 			  </div>
 			  <div className="SearchBar-fields">
-			    <input placeholder="Search Businesses" />
-			    <input placeholder="Where?" />
+			    <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+			    <input onChange={this.handleLocationChange} placeholder="Where?" />
 			  </div>
 			  <div className="SearchBar-submit">
-			    <a>Let's Go</a>
+			    <a onClick={this.handleSearch}>Let's Go</a>
 			  </div>
 			</div>
 		)
